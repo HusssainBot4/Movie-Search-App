4d5c41d93a4ea0c43d58ba0630dfd2f0de3346e4
HusssainBot4 <hussainahmedgoat@gmail.com>
2026-07-09 22:48:49 +0500

    Added events listeners to movies cards

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import { debounce } from "../utils/debounce.js";
 
 const searchInput = document.querySelector("#searchInput");
 const status = document.querySelector("#status");
+const moviesGrid = document.querySelector("#moviesGrid");
 
 const debouncedSearch = debounce(handleSearch, 300);
 searchInput.addEventListener(
@@ -12,6 +13,41 @@ searchInput.addEventListener(
     debouncedSearch
 );
 
+moviesGrid.addEventListener( "click", handleMovieClick );
+
+async function handleMovieClick(event) {
+
+    const card =
+        event.target.closest(".movie-card");
+
+    if (!card) return;
+
+    const movieId =
+        card.dataset.id;
+
+    try {
+        const movie =
+            await getMovieDetails(movieId);
+        renderModal(movie);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+dialog.addEventListener("click", (event) => {
+
+    const rect =
+        dialog.getBoundingClientRect();
+    const clickedOutside =
+        event.clientX < rect.left ||
+        event.clientX > rect.right ||
+        event.clientY < rect.top ||
+        event.clientY > rect.bottom;
+    if (clickedOutside) {
+        dialog.close();
+    }
+});
 
 
 async function handleSearch(event) {
